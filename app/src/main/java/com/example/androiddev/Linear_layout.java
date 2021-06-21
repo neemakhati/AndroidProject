59949d19a0ef87ff365905e4764d047a3d385621
neemakhati <neemakhati11@gmail.com>
2021-06-21 20:57:33 +0545

    Branch : Linear Layout

---
+++ app/src/main/java/com/example/androiddev/Linear_layout.java
@@ -0,0 +1,15 @@
+package com.example.androiddev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Linear_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+
+    }
+}
