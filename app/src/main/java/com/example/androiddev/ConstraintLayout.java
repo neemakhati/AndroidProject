825fad253eebb777792bb91f7ca5947160148118
neemakhati <neemakhati11@gmail.com>
2021-07-02 20:51:48 +0545

    Forth branch: Linear Layout intent

--- app/src/main/java/com/example/androiddev/ConstraintLayout.java
+++ app/src/main/java/com/example/androiddev/ConstraintLayout.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
-public class Relative_layout extends AppCompatActivity {
+public class ConstraintLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative_layout);
+        setContentView(R.layout.activity_constraint_layout);
     }
 }
--- app/src/main/java/com/example/androiddev/Linear_layout.java
+++ app/src/main/java/com/example/androiddev/Linear_layout.java
@@ -3,13 +3,25 @@ package com.example.androiddev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class Linear_layout extends AppCompatActivity {
+
+    private TextView linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String statePassedFromMainActivity = extras.getString("destination");
+            System.out.println("debug " + statePassedFromMainActivity);
+            linear = findViewById(R.id.lineartext);
+            linear.setText(statePassedFromMainActivity);
+        }
 
     }
 }
